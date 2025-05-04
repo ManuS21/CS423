500f2bc64256c7e33c6b5fce1d734a5f60092caf
Mshukla <38549274+ManuS21@users.noreply.github.com>
2025-05-03 17:09:16 -0700

    Update library.py

--- library.py
+++ library.py
@@ -10,6 +10,7 @@ import warnings
 sklearn.set_config(transform_output="pandas") #says pass pandas tables through pipeline instead of numpy matrices
 from sklearn import set_config  #add both of these to your library before you define any transformers
 set_config(transform_output="pandas")
+from sklearn.neighbors import KNeighborsClassifier
 
 titanic_variance_based_split = 107
 customer_variance_based_split = 113
