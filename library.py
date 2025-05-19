8b5bfd136fb3514e96f10d7ffac0285e90d1b9ac
Mshukla <38549274+ManuS21@users.noreply.github.com>
2025-05-19 14:51:50 -0700

    Update library.py

--- library.py
+++ library.py
@@ -1026,17 +1026,18 @@ def threshold_results(thresh_list, actuals, predicted):
 
 
 def halving_search(model, grid, x_train, y_train, factor=2, min_resources="exhaust", scoring='roc_auc'):
-  halving_cv = HalvingGridSearchCV(
-    model, grid,
-    scoring = scoring,
-    n_jobs=-1,
-    min_resources = min_resources,
-    factor = factor,
-    cv=5, random_state=1234,
-    refit=True,
-)
-grid_result = halving_cv.fit(x_train, y_train)
-return grid_result
+    halving_cv = HalvingGridSearchCV(
+        model, grid,
+        scoring = scoring,
+        n_jobs=-1,
+        min_resources = min_resources,
+        factor = factor,
+        cv=5, random_state=1234,
+        refit=True,
+    )
+
+    grid_result = halving_cv.fit(x_train, y_train)
+    return grid_result
 
 
 def sort_grid(grid):
